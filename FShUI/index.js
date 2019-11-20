0fd75800fe67d35955533506099da4eeb6d4878a
amieres <amieres@hotmail.com>
2019-11-19 18:30:19 -0600

    added marked.js

--- FShUI/index.js
+++ FShUI/index.js
@@ -1,8 +1,9 @@
 requirejs([
       "https://code.jquery.com/jquery-3.1.1.min.js"
     , "/EPFileX/FileSaver/FileSaver.js"
+    , "https://cdn.jsdelivr.net/npm/marked/marked.min.js"
     , "testing"
 ],
-function   (jquery, fileSaver, FShUI) {
-    ;
+function   (jquery, fileSaver, markedJS, FShUI) {
+    marked = markedJS;
 });
